a79e00233011280669a7b01104b9a4c1252efd0e
Thomas O'Brien <thomas.obrien86@gmail.com>
2021-12-01 17:07:12 +0000

    tidyup

--- common/src/io/mod.rs
+++ common/src/io/mod.rs
@@ -2,12 +2,10 @@ use std::{io::BufRead, str::FromStr};
 
 pub fn collect_stdin<T: FromStr>() -> Vec<T> {
     let stdin = std::io::stdin();
-    let values = stdin
+    stdin
         .lock()
         .lines()
         .filter_map(|line| line.ok().map(|line| line.parse::<T>()))
         .filter_map(|value| value.ok())
-        .collect::<_>();
-
-    return values
+        .collect::<_>()
 }
