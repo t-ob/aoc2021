2db705c1de764ffe7cece1074361b7d902b2c404
Thomas O'Brien <thomas.obrien86@gmail.com>
2021-12-06 15:09:18 +0000

    Move iteration logic out of advance

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,18 +3,12 @@ use common::io::stdin::read_line_with;
 static PART_1_DAYS: usize = 80;
 static PART_2_DAYS: usize = 256;
 
-fn advance(iterations: usize, data: &mut [usize; 9]) {
-    let mut iteration = 0;
-
-    while iteration < iterations {
-        let next_zero = data[6] + data[8];
-        for i in (1..9).rev() {
-            data[i] = data[i - 1];
-        }
-        data[0] = next_zero;
-
-        iteration += 1;
+fn advance(data: &mut [usize; 9]) {
+    let next_zero = data[6] + data[8];
+    for i in (1..9).rev() {
+        data[i] = data[i - 1];
     }
+    data[0] = next_zero;
 }
 
 fn scalar_product(lhs: &[usize; 9], rhs: &[usize; 9]) -> usize {
@@ -39,10 +33,18 @@ fn main() {
 
         let mut dp = [1; 9];
 
-        advance(PART_1_DAYS, &mut dp);
+        let mut iteration = 0;
+
+        while iteration < PART_1_DAYS {
+            advance( &mut dp);
+            iteration += 1;
+        }
         println!("{}", scalar_product(&counts, &dp));
 
-        advance(PART_2_DAYS - PART_1_DAYS, &mut dp);
+        while iteration < PART_2_DAYS {
+            advance(&mut dp);
+            iteration += 1;
+        }
         println!("{}", scalar_product(&counts, &dp));
     }
 }
