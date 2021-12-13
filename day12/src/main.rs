090f091e07d6339f0accd55e2c12f79b23b6fddf
Tom O'Brien <thomas.obrien86@gmail.com>
2021-12-13 10:54:23 +0000

    part1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,110 +1,78 @@
 use std::{collections::{HashMap, HashSet}, str::FromStr};
 
-const START: u8 = 0;
-const END: u8 = u8::MAX;
-
 use common::io::stdin;
 
-fn construct_candidates(path: &[u8; 1 << 8], k: usize, graph: &HashMap<u8, Vec<u8>>, candidates: &mut [u8; 1 << 8], nc: &mut usize) {
-    let mut in_sol = [false; 1 << 8];
+type Vertex = String;
+type Graph = HashMap<Vertex, Vec<Vertex>>;
+type Path = Vec<Option<Vertex>>;
+
+fn construct_candidates(path: &Path, k: usize, graph: &Graph, candidates: &mut Vec<Option<Vertex>>, nc: &mut usize) {
+    let mut in_sol = HashSet::new(); // [false; 1 << 8];
     for i in 0..k {
-        let c = path[i] as char;
-        in_sol[c as usize] = !(c.is_ascii_alphabetic() && c.is_uppercase());
+        if let Some(s) = path[i].clone() {
+            if !(s == s.to_uppercase()) {
+                in_sol.insert(s);
+            }
+        }
     }
 
-    if k == 0 {
-        candidates[0] = START;
+    if k == 1 {
+        candidates[0] = Some("start".to_string());
         *nc = 1;
         return;
     }
 
     *nc = 0;
-    let last = path[k-1];
-    if last == END {
-        return;
-    }
-    if let Some(ns) = graph.get(&last) {
-        for v in ns {
-            if !in_sol[*v as usize] {
-                candidates[*nc] = *v;
-                *nc += 1;
+    if let Some(last) = path[k-1].clone() {
+        if last == "end".to_string() {
+            return
+        }
+        if let Some(ns) = graph.get(&last) {
+            for v in ns {
+                if !in_sol.contains(v) {
+                    candidates[*nc] = Some(v.clone());
+                    *nc += 1;
+                }
             }
         }
     }
-
-    //print_path(path, k);
 }
 
-fn backtrack(path: &mut [u8; 1 << 8], k: usize, graph: &HashMap<u8, Vec<u8>>, solution_count: &mut usize) {
-    let mut candidates = [0; 1 << 8];
+fn backtrack(path: &mut Path, k: usize, graph: &Graph, solution_count: &mut usize) {
+    let mut candidates = vec![None; 1 << 8];
     let mut nc = 0;
-    if path[k-1] == END {
-        print_path(&path, k);
-
+    if path[k-1] == Some("end".to_string()) {
         *solution_count += 1;
         return;
     }
     construct_candidates(path, k, graph, &mut candidates, &mut nc);
 
     for i in 0..nc {
-        path[k] = candidates[i];
+        path[k] = candidates[i].clone();
         backtrack(path, k + 1, graph, solution_count);
     }
-}
-
-// fn vertex_as_u8(s: &str) -> u8 {
-//     match s {
-//         "start" => 0,
-//         "end" => u8::MAX,
-//         _ => s.chars().fold(init, f)
-//         _ => s.chars().next().unwrap() as u8
-//     }
-// }
-
-fn label(u: u8) -> String {
-    match u {
-        0 => "start".to_string(),
-        u8::MAX => "end".to_string(),
-        b => format!("{}", (b as char)),
-    }
-}
-
-fn print_path(path: &[u8; 1 << 8], k: usize) {
-    let mut nodes = Vec::new();
-    for i in 0..k {
-        let n = label(path[i]);
-        nodes.push(n);
-        if path[i] == u8::MAX {
-            break;
-        }
-    }
-
-    println!("{}", nodes.join(" -> "));
 }
 
 fn main() {
     let edges = stdin::collect_into_vec_with(|line| {
         let mut tokens = line.trim().split('-');
         match (tokens.next(), tokens.next()) {
-            (Some(u), Some(v)) => Some((vertex_as_u8(u), vertex_as_u8(v))),
+            (Some(u), Some(v)) => Some((u.to_string(), v.to_string())),
             _ => None
         }});
 
-    let mut graph = HashMap::new();
+    let mut graph = Graph::new();
     for (u, v) in edges {
-        let neighbours_u = graph.entry(u).or_insert(Vec::new());
-        neighbours_u.push(v);
-        let neighbours_v = graph.entry(v).or_insert(Vec::new());
-        neighbours_v.push(u);
+        let neighbours_u = graph.entry(u.clone()).or_insert(Vec::new());
+        neighbours_u.push(v.clone());
+        let neighbours_v = graph.entry(v.clone()).or_insert(Vec::new());
+        neighbours_v.push(u.clone());
     }
 
-    println!("{:?}", graph.iter().map(|(k, v)| (label(*k), v.iter().map(|u| label(*u)).collect::<Vec<_>>())).collect::<HashMap<_, _>>());
-
-    let mut path = [0; 1 << 8];
+    let mut path = vec![None; 1 << 8];
     let mut solution_count = 0;
 
     backtrack(&mut path, 1, &graph, &mut solution_count);
     
-
     println!("{}", solution_count);
 }
